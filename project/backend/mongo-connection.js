7959e76e9815f28f05ebd08ab02d29791f059edc
Natalia Macari <macari.app.dev@gmail.com>
2019-12-09 23:11:28 +0100

    update mongo-connection.js

--- project/backend/mongo-connection.js
+++ project/backend/mongo-connection.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 
 async function main(){
-    mongoose.da
-    await mongoose.connect('mongodb://localhost/onlinefoodservice', { useNewUrlParser: true, useUnifiedTopology: true })
+    await mongoose.connect(
+    process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost/onlinefoodservice', 
+    { useNewUrlParser: true, useUnifiedTopology: true })
     console.log('mongoose connected')
 }
 
