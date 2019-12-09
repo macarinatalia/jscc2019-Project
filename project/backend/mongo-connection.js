21f18fac81aeebe7944c92185f5984bba141d2cc
Natalia Macari <macari.app.dev@gmail.com>
2019-12-10 00:00:02 +0100

    playing with mongoose

--- project/backend/mongo-connection.js
+++ project/backend/mongo-connection.js
@@ -2,9 +2,13 @@ const mongoose = require('mongoose')
 
 async function main(){
     await mongoose.connect(
-    process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost/onlinefoodservice', 
-    { useNewUrlParser: true, useUnifiedTopology: true })
-    console.log('mongoose connected')
+        process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost/onlinefoodservice', 
+        { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => console.log('mongoose connected')
+        .catch(err => {
+            console.log(Error, err.message);
+        })
+    
 }
 
 main()
