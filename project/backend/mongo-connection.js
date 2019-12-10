3038043ef44b6e99e051d32ab872581e73efa0e7
Natalia Macari <macari.app.dev@gmail.com>
2019-12-10 12:14:16 +0100

    additonal process.env value

--- project/backend/mongo-connection.js
+++ project/backend/mongo-connection.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose')
 
 async function main(){
-    await mongoose.connect(
-        process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost/onlinefoodservice', 
-        { useNewUrlParser: true, useUnifiedTopology: true })
-    console.log('mongoose connected')
-        
-    
+    try {
+        await mongoose.connect(
+            process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost/onlinefoodservice', 
+            { useNewUrlParser: true, useUnifiedTopology: true })
+        console.log('mongoose connected')
+    } catch (error) {
+        console.log('mongoose is NOT connected')
+        handleError(error)
+    }
 }
 
 main()
